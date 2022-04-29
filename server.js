5fe1b6a886ea9aa874271c2587aa540bf2d3d28f
Raisov-Na <101696850+Raisov-Na@users.noreply.github.com>
2022-04-29 10:18:22 +0300

    Хранение списка задач

--- server.js
+++ server.js
@@ -1,25 +1,45 @@
 var express = require("express"),
-	http = require("http"),
-	app = express(),
-	toDos = [
-		// настраиваем список задач копированием
-		// содержимого из файла todos.OLD.json
-	];
+http = require("http"),
+app = express(),
+toDos = [
+    {
+        "description": "Купить продукты",
+        "tags": ["Шопинг", "Рутина"]
+    },
+    {
+        "description": "Сделать несколько новых задач",
+        "tags": ["Писательство", "Работа"]   
+    },
+    {
+        "description": "Подготовиться к лекции в понедельник",
+        "tags": ["Работа", "Преподавание"]  
+    },
+    {
+        "description": "Ответить на электронные письма",
+        "tags": ["Работа"]  
+    },
+    {
+        "description": "Вывести Грейси на прогулку в парк",
+        "tags": ["Рутина", "Питомцы"]  
+    },
+    {
+        "description": "Закончить писать книгу",
+        "tags": ["Писательство", "Работа"]  
+    }
+];
+
 app.use(express.static(__dirname + "/client"));
 http.createServer(app).listen(3000);
-// Этот маршрут замещает наш файл
-// todos.json в примере из части 5
-app.get("/todos.json", function (req, res) {
-	res.json(toDos);
+app.get("/todos.json", function(req, res) {
+    res.json(toDos);
 });
-// командуем Express принять поступающие
-// объекты JSON
+// командуем Express принять поступающие объекты JSON
 app.use(express.urlencoded({ extended: true }));
-app.post("/todos", function (req, res) {
-	// сейчас объект сохраняется в req.body
-	var newToDo = req.body;
-	console.log(newToDo);
-	toDos.push(newToDo);
-	// отправляем простой объект
-	res.json({"message":"Вы размещаетеся на сервере!"});
-});
+app.post("/todos", function(req, res) {
+    // сейчас объект сохраняется в req.body
+    var newToDo = req.body;
+    console.log(newToDo);
+    toDos.push(newToDo);
+    console.log("Данные были отправлены на сервер!");
+    res.json({"message":"Вы размещаетесь на сервере!"});
+})
