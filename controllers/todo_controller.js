949c056e7e12a6cdd208e01f23e8a3c7b535df0d
Raisov-Na <101696850+Raisov-Na@users.noreply.github.com>
2022-05-07 11:42:47 +0300

    Удаление элементов из списка задач

--- controllers/todo_controller.js
+++ controllers/todo_controller.js
@@ -1,5 +1,4 @@
 var ToDo = require("../models/ToDo.js"),
-	mongoose = require("mongoose");
 	ToDosController = {};
 
 ToDosController.index = function (req, res) {
@@ -16,7 +15,6 @@ ToDosController.index = function (req, res) {
 		});
 	};
 	if (username !== null) {
-		console.log("Поиск пользователя: "+username);
 		User.find({"username": username}, function (err, result) {
 			if (err !== null) {
 				res.json(500, err);
@@ -30,7 +28,6 @@ ToDosController.index = function (req, res) {
 		respondWithToDos({});
 	}
 };
-
 ToDosController.create = function (req, res) {
 	var username = req.params.username || null;
 	var newToDo = new ToDo({
@@ -38,7 +35,6 @@ ToDosController.create = function (req, res) {
 		"tags": req.body.tags
 	});
 
-	console.log("username: " + username);
 
 	User.find({"username": username}, function (err, result) {
 		if (err) {
@@ -64,13 +60,38 @@ ToDosController.create = function (req, res) {
 };
 
 ToDosController.show = function (req, res) {
-	console.log("вызвано действие: показать");
-	res.send(200);
+	// это ID, который мы отправляем через URL
+	var id = req.params.id;
+	// находим элемент списка задач с соответствующим ID
+	ToDo.find({"_id":id}, function (err, todo) {
+		if (err !== null) {
+			// возвращаем внутреннюю серверную ошибку
+			res.status(500).json(err);
+		} else {
+			if (todo.length > 0) {
+				// возвращаем успех!
+				res.status(200).json(todo[0]);
+			} else {
+				// мы не нашли элемент списка задач с этим ID!
+				res.send(404);
+			}
+		}
+	});
 };
 
 ToDosController.destroy = function (req, res) {
-	console.log("destroy action called");
-	res.send(200);
+	var id = req.params.id;
+	ToDo.deleteOne({"_id": id}, function (err, todo) {
+		if (err !== null) {
+			res.status(500).json(err);
+		} else {
+			if (todo.n === 1 && todo.ok === 1 && todo.deletedCount === 1) {
+				res.status(200).json(todo);
+			} else {
+				res.status(404).json({"status": 404});
+			}
+		}
+	});
 }
 
 ToDosController.update = function (req, res) {
